fe9f05dd6ffd088edea24de32b42c2525edd6f8d
lorenzo99-droid <lorenzoferr.mar@gmail.com>
2021-02-22 17:49:47 -0300

    Subindo tarefa bons drinks usando grid

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Components/Header.js';
+import Menu from './Components/Menu';
+function App() {
+
+  return (
+    <div className="drinks_app">
+      <Header></Header>
+      <Menu></Menu>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Teste.js
@@ -0,0 +1,13 @@
+Teste.js
+import React, { Component } from 'react'
+
+export default class Teste extends Component {
+    render() {
+        //classes
+        return (
+            <div >
+                
+            </div>
+        )
+    }
+}
