56603e432dc45878ec5a8f4e66687c9a4c10e38b
lorenzo99-droid <lorenzoferr.mar@gmail.com>
2021-03-28 02:01:45 -0300

    página contatos estilizada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
 import Header from './Components/Header/Header.js';
 import Menu from './Components/Menu/Menu.js';
-import Button from './button/Button_test_css';
-import {
-  BrouserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Button from './button/Button_test_css'
 function App() {
 
   return (
@@ -19,37 +13,4 @@ function App() {
   );
 }
 
-export default function App(){
-  return(
-    <Router>
-      <div>
-        <nav>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/sobre">Home</Link>
-          </li>
-          <li>
-          <Link to="/contato">Home</Link>
-          </li>
-        </nav>
-      </div>
-    
-
-      <Switch>
-         <Route exact path="/">
-            <home />
-          </Route>
-          <Route path="/contato">
-            <contat />
-          </Route>
-          <Route path="/*">
-            <contat />
-          </Route>
-      </Switch>
-    </Router>
-  )
-};
-
-
+export default App;
--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react'
-import style from "./menu.module.css"
+import React, { Component } from "react";
+import style from "./menu.module.css";
 export default class Menu extends Component {
-    render() {
-        return (
-            <body>
-                <h2>CONTATO</h2>
-                <div className={style.formContainer}>
-                    <img className={style.imagem} src="https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg"></img>
-                    <form className={style.forms}>
-                        <div className={style.campoNome}>
-                            <label >nome: </label>
-                            <input className={style.css}/>
-                        </div>
-                        <div className={style.campoEmail}>
-                            <label>e-mail: </label>
-                            <input className={style.css}/>
-                        </div>
+  render() {
+    return (
+      <div>
+        <h2>CONTATO</h2>
+        <div className={style.formContainer}>
+          <img
+            className={style.imagem}
+            src="https://bonsdrinks.netlify.app/static/media/contact-pic.689e126c.jpg"
+          ></img>
 
-                        <label className={style.mensagem}>mensagem: </label>
-                        <textarea  name="mensagem"/>
-                        <div>
-                            <button className={style.botao}>Send</button>
-                        </div>
-                    </form>
-                </div>
-            </body>
-        )
-    }
+          <form className={style.forms}>
+              <input placeholder="Nome"/>
+            
+             <input placeholder="E-mail"/>
+
+            <textarea placeholder="Mensagem" name="mensagem" />
+            
+            <button className={style.botao}>Enviar</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
