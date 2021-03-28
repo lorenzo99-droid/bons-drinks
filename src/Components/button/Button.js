49684b11c475d11ebd9a5e80d67b164d6314568b
lorenzo99-droid <lorenzoferr.mar@gmail.com>
2021-03-28 17:35:34 -0300

    botao disabled

--- src/Components/button/Button.js
+++ src/Components/button/Button.js
@@ -1,8 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import style from './button.module.css';
 
 export default function Button(props){
-    return (
-        <button className={style.button} style={{width: props.largura}}>{props.children}</button>
+
+const [disabled, setDisabled] = useState(false)
+
+const desabilitar = () =>{
+    console.log("clique ok")
+    setDisabled(true)
+    setTimeout(() =>{setDisabled(false)},3000)
+}
+
+function componentDidUpdate(){
+    console.log(`A função disabled é ${disabled}`)
+}
+return (
+        <button className={style.button} style={{width: props.largura}}  disabled={disabled} onClick={desabilitar}>{props.children}</button>
     )
 }
